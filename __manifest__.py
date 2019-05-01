1e7f88e7871f26c3f2d2548774ad8d48d3accbee
Antonio <aolmolopez97@gmail.com>
2019-05-01 22:02:15 +0200

    Subida

--- __manifest__.py
+++ __manifest__.py
@@ -26,6 +26,7 @@
     'data': [
         # 'security/ir.model.access.csv',
         'views/socios.xml',
+        'views/deportes.xml','
         'views/instalaciones.xml',
     ],
     # # only loaded in demonstration mode
